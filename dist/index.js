9e09f3cc509a0e712572ac8ee35309a707ddeb24
unknown <habigalexander91@gmail.com>
2023-07-27 12:23:06 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const body_parser_1 = __importDefault(require("body-parser"));
 const compression_1 = __importDefault(require("compression"));
+require("dotenv/config");
 // import cookieParser from 'cookie-parser';
 const cors_1 = __importDefault(require("cors"));
 const router_1 = __importDefault(require("./router"));
@@ -12,7 +13,7 @@ const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
-const allowedOrigins = ['http://localhost:3000', "https://todo-list-six-sand.vercel.app"];
+const allowedOrigins = ['http://localhost:3000', 'https://todo-list-six-sand.vercel.app'];
 const corsOptions = {
     origin: (origin, callback) => {
         if (allowedOrigins.includes(origin)) {
@@ -31,8 +32,11 @@ const PORT = process.env.PORT;
 // server.listen(3001, () => {
 //     console.log(`Server running on http://localhost:3001/`)
 // });
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
 app.listen(PORT, () => {
-    console.log(`Server running on Port ${PORT}`);
+    console.log(`Server running on Port ` + PORT);
 });
 const MONGO_URL = 'mongodb+srv://xalex:xalex@cluster0.aawauya.mongodb.net/?retryWrites=true&w=majority';
 mongoose_1.default.Promise = Promise;
